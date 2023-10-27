f7f5d1f78cf1ab8e471510371ddb5489607cb14e
julinhaaaaaaaaaaaa <131277580+julinhaaaaaaaaaaaa@users.noreply.github.com>
2023-10-27 12:31:12 -0300

    Add files via upload

---
+++ ConversãoDeTiposDeVariaveis.js
@@ -0,0 +1,7 @@
+console.log("2"+"2")
+console.log(parseInt("2")+parseInt("2")
+console.log("10"/"2");//mostrar 102
+console.log("nome"/"2");// not a number
+console.log(6-5),// imprime 6.5
+console.log(6,5);
+
